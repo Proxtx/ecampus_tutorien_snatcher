9b168953c61d61ed8155dda84034b777e018e886
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-10-13 23:47:18 +0200

    forgot bracket and async

--- past_me.js
+++ past_me.js
@@ -1,6 +1,8 @@
-(() => while((new Date()).getMinutes() != YOUUNLOCKMINUTE) await new Promise(r=>setTimeout(r, 10));
-for(let i = 0;i<5;i++) {
-  await fetch("yoururl", {method: "POST"})
-  await new Promise(r => setTimeout(r, 100))
-}
-})()
+(async () => {
+  while (new Date().getMinutes() != YOUUNLOCKMINUTE)
+    await new Promise((r) => setTimeout(r, 10));
+  for (let i = 0; i < 5; i++) {
+    await fetch("yoururl", { method: "POST" });
+    await new Promise((r) => setTimeout(r, 100));
+  }
+})();
