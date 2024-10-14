53dd749aac9ba8777a57155fc7de0509f4e6d1a5
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-10-14 23:57:15 +0200

    add url and formatting

--- past_me.js
+++ past_me.js
@@ -1,8 +1,11 @@
 (async () => {
-  while (new Date().getMinutes() != YOUUNLOCKMINUTE)
+  while (new Date().getMinutes() != 0)
     await new Promise((r) => setTimeout(r, 10));
   for (let i = 0; i < 5; i++) {
-    await fetch("yoururl", { method: "POST" });
+    await fetch(
+      "ilias.php?ref_id=COURSE_ID&cmd=post&cmdClass=ilgroupregistrationgui&cmdNode=wi:nj:dj&baseClass=ilRepositoryGUI&fallbackCmd=join&rtoken=TOKEN",
+      { method: "POST" }
+    );
     await new Promise((r) => setTimeout(r, 100));
   }
 })();
